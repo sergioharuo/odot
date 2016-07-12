1e8a1c5ab45af8075d50f01b35b738c7c24c55b3
Sergio Haruo <sergioharuo@gmail.com>
2016-07-11 21:28:08 -0300

    add validations for title and description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -29,4 +29,58 @@ describe "Create todo lists" do
     	visit "/todo_lists"
     	expect(page).to_not have_content("This is a sucess today, Capybara POWERRR")
     end
+
+        it "displays error when the todo list has a title with less than 3 characters" do
+    	expect(TodoList.count).to eq(0)
+
+    	visit "/todo_lists"
+        click_link "New Todo List"
+        expect(page).to have_content("New Todo List")
+
+        fill_in "Title", with: "Hi"
+        fill_in "Description", with: "This is a sucess today, Capybara POWERRR"
+    	click_button "Create Todo list"
+        
+        expect(page).to have_content("error")
+    	expect(TodoList.count).to eq(0)
+
+    	visit "/todo_lists"
+    	expect(page).to_not have_content("This is a sucess today, Capybara POWERRR")
+    end
+
+            it "displays error when the todo list has no Description" do
+    	expect(TodoList.count).to eq(0)
+
+    	visit "/todo_lists"
+        click_link "New Todo List"
+        expect(page).to have_content("New Todo List")
+
+        fill_in "Title", with: "Grocery list"
+        fill_in "Description", with: ""
+    	click_button "Create Todo list"
+        
+        expect(page).to have_content("error")
+    	expect(TodoList.count).to eq(0)
+
+    	visit "/todo_lists"
+    	expect(page).to_not have_content("Grocery list")
+    end
+
+        it "displays error when the todo list has a Description with less than 5 characters" do
+    	expect(TodoList.count).to eq(0)
+
+    	visit "/todo_lists"
+        click_link "New Todo List"
+        expect(page).to have_content("New Todo List")
+
+        fill_in "Title", with: "Grocery list"
+        fill_in "Description", with: "Capy"
+    	click_button "Create Todo list"
+        
+        expect(page).to have_content("error")
+    	expect(TodoList.count).to eq(0)
+
+    	visit "/todo_lists"
+    	expect(page).to_not have_content("This is a sucess today, Capybara POWERRR")
+    end
 end
